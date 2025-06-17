8da218801776e76086979617acce572625142bac
dev-m-joshua <mutambukijoshua2@gmail.com>
2025-06-17 23:24:54 +0300

    Fetching cars from the database

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GetCars from "./Components/cars/GetCars";
+
+function App() {
+
+  return (
+    <>
+      <GetCars />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Types/CarType.ts
@@ -0,0 +1,9 @@
+export interface CarType {
+    carID: number;
+    carModel: string;
+    year: Date;
+    color: string;
+    rentalRate: number;
+    availability: boolean;
+    locationID: number;
+}
